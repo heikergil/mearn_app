0be519edb2371e63e7991221bff1990db280ce9b
Heiker Gil <gilheyker@gmail.com>
2021-08-29 16:33:22 -0500

    setup

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 // app.js
 
 const express = require('express');
+const connectDB = require('./config/db');
+// routes
+const books = require('./routes/api/books');
 
 const app = express();
+const morgan = require('morgan')
+
+// Connect Database
+connectDB();
+
+app.use(morgan("tiny"));
 
 app.get('/', (req, res) => res.send('Hello world!'));
+
+// use Routes
+app.use('/api/books', books);
 
 const port = process.env.PORT || 8082;
 
